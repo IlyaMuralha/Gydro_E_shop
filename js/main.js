e45e05007ba0e915f266c0c111f449240e054c19
IlyaMuralha <muralhakuzmenko@gmail.com>
2024-02-03 21:03:32 +0500

    init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $(".promo__slider").slick({
+        dots: true,
+        prevArrow: '<button class="promo__slider-btn promo__slider-btnprev"><img src="icons/arrow_left.png" alt=""></button>',
+        nextArrow: '<button class="promo__slider-btn promo__slider-btnnext"><img src="icons/arrow_right.png" alt=""></button>'
+    });
+});
